a558ba0837610122c8cdcfc0741c33c3f4085ba6
Claudio Junior <Cjrnew@gmail.com>
2021-07-31 14:37:57 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/cjrnew/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjrnew.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
